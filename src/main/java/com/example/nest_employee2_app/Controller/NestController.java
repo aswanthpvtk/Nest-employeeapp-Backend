0df4dda1484e3f027476254858a0eea97da26ec4
Aswanth P <Aswanth@NTP-LAP-734.chn.nestgroup.net>
2022-12-12 00:57:00 +0530

    n

--- src/main/java/com/example/nest_employee2_app/Controller/NestController.java
+++ src/main/java/com/example/nest_employee2_app/Controller/NestController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.HashMap;
+import java.util.List;
 
 @RestController
 public class NestController {
@@ -24,6 +25,30 @@ public class NestController {
         HashMap<String, String> map = new HashMap<>();
         map.put("status","success");
         return map;
+    }
+
+
+    @CrossOrigin(origins = "*")
+    @PostMapping(path = "/userLogin", produces = "application/json", consumes = "application/json")
+    public HashMap<String, String> UserLogin(@RequestBody Employee u)
+    {
+        System.out.println(u.getEmail());
+        System.out.println(u.getPsw());
+        List<Employee> result = (List<Employee>) dao.login(u.getEmail(), u.getPsw());
+        HashMap<String, String> st = new HashMap<>();
+        if (result.size() == 0)
+        {
+            st.put("status", "failed");
+            st.put("message", "user doesn't exist");
+        } else
+        {
+            int id = result.get(0).getId();
+            st.put("userId", String.valueOf(id));
+            st.put("status", "success");
+            st.put("message", "success");
+        }
+
+        return st;
     }
 
 }
--- src/main/java/com/example/nest_employee2_app/dao/EmployeeDao.java
+++ src/main/java/com/example/nest_employee2_app/dao/EmployeeDao.java
@@ -1,7 +1,14 @@
 package com.example.nest_employee2_app.dao;
 
 import com.example.nest_employee2_app.model.Employee;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface EmployeeDao extends CrudRepository<Employee,Integer> {
+
+    @Query(value = "SELECT `id`, `designation`, `email`, `empid`, `mob`, `name`, `psw`, `salary`, `uname` FROM `employees` WHERE `email`=:email AND `psw`=:psw",nativeQuery = true)
+    List<Employee> login(@Param("email") String email, @Param("psw")String psw);
 }
